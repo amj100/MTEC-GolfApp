42793d58d228b6d1ae0f9a2e3496376f603729b3
Andrew Jacob <amj1108@icloud.com>
2020-05-04 08:20:04 -0600

    page connection

--- src/app/scorecard/scorecard.component.ts
+++ src/app/scorecard/scorecard.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { GolfCoursesApiService } from '../services/golf-courses-api.service';
+import { ScorecardService } from '../services/scorecard.service';
+import { Router } from '@angular/router';
+import { Player } from '../interfaces/player';
 
 @Component({
-  selector: 'app-scorecard',
-  templateUrl: './scorecard.component.html',
-  styleUrls: ['./scorecard.component.css']
+	selector: 'app-scorecard',
+	templateUrl: './scorecard.component.html',
+	styleUrls: ['./scorecard.component.css']
 })
 export class ScorecardComponent implements OnInit {
+	players: Player[] = []
+	scorecard: object = {}
+	tee: string = ""
 
-  constructor() { }
+	constructor(
+		public golfCoursesApiService: GolfCoursesApiService,
+		public scorecardService: ScorecardService,
+		private router: Router
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		if (this.scorecardService.scorecardReady()) {
+			this.tee = this.scorecardService.tee
+			this.players = this.scorecardService.players
+			this.scorecardService.getScorecard().subscribe(data => {
+				this.scorecard = data["data"] ? data["data"] : data
+			})
+		}
+		else {
+			this.router.navigate(["/setup"])
+		}
+	}
 
 }
--- src/app/services/scorecard.service.ts
+++ src/app/services/scorecard.service.ts
@@ -4,10 +4,12 @@ import { HttpClient } from '@angular/common/http';
 import { Course } from '../interfaces/course';
 import { Player } from '../interfaces/player';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ScorecardService {
+	scorecardObservable: Observable<object>
 	players: Player[] = []
 	course: string = ""
 	tee: string = ""
@@ -16,7 +18,15 @@ export class ScorecardService {
 	constructor(
 		private http: HttpClient,
 	) { }
+	scorecardReady(): boolean {
+		if (this.players.length > 0 && this.course != "" && this.tee != "") {
+			return true
+		}
+		else {
+			return false
+		}
+	}
 	getScorecard(): Observable<object> {
-		return this.http.get("https://golf-courses-api.herokuapp.com/courses" + this.course)
+		return this.http.get("https://golf-courses-api.herokuapp.com/courses/" + this.course)
 	}
 }
