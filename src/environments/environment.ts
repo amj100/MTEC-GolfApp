cfea9891cd38dd5693e62103acd8201d1c282e4e
Andrew Jacob <amj1108@icloud.com>
2020-05-05 13:34:37 -0600

    hide api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,6 @@
 export const environment = {
 	production: false,
 	firebase: {
-		apiKey: "AIzaSyBVJzu2B2096smwPTXAizaLhi8I8EmoKJ0",
-		authDomain: "mtec-golfapp.firebaseapp.com",
-		databaseURL: "https://mtec-golfapp.firebaseio.com",
-		projectId: "mtec-golfapp",
-		storageBucket: "mtec-golfapp.appspot.com",
-		messagingSenderId: "732697579236",
-		appId: "1:732697579236:web:cda3f5e22420a3525ee6f1"
 	}
 }
 
