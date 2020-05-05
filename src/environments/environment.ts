423b5fbfcdca3572dc8c69c22ed826ed3d9c3426
Andrew Jacob <amj1108@icloud.com>
2020-05-05 11:57:42 -0600

    add environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyBVJzu2B2096smwPTXAizaLhi8I8EmoKJ0",
+		authDomain: "mtec-golfapp.firebaseapp.com",
+		databaseURL: "https://mtec-golfapp.firebaseio.com",
+		projectId: "mtec-golfapp",
+		storageBucket: "mtec-golfapp.appspot.com",
+		messagingSenderId: "732697579236",
+		appId: "1:732697579236:web:cda3f5e22420a3525ee6f1"
+	}
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
